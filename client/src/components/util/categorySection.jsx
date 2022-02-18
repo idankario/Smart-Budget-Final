173b8e22da4edb52de752a0941f63a4a10028333
mhmdxx5 <48560391+mhmdxx5@users.noreply.github.com>
2022-02-19 00:51:19 +0200

    conflict

--- client/src/components/util/categorySection.jsx
+++ client/src/components/util/categorySection.jsx
@@ -4,16 +4,16 @@ import Home from '../images/Home.png';
 import Groceries from '../images/Groceries.png';
 import { ProgressStyle, FlexSection, H5styles } from '../board';
 
-const CategorySection = ({ monthExpenses,budgetLimit}) => {
-  const getTotalCost =  (obj, value) => {
+const CategorySection = ({ monthExpenses, budgetLimit }) => {
+  const getTotalCost = (obj, value) => {
     let count = 0;
     const newobj = [];
-    Object.keys(obj).map( (k)=> {
+    Object.keys(obj).map((k) => {
       if (obj[k].category === value) {
         newobj.push(obj[k]);
       }
     })
-    Object.keys(newobj).map( (s) =>{
+    Object.keys(newobj).map((s) => {
       count = count + newobj[s].cost;
     })
     return count;
@@ -23,29 +23,27 @@ const CategorySection = ({ monthExpenses,budgetLimit}) => {
     {
       src: Taxi,
       name: "Public transport",
-      value: getTotalCost(monthExpenses, "Public transport")/budgetLimit
+      value: getTotalCost(monthExpenses, "Public transport") / budgetLimit
     },
 
     {
       src: Sport,
       name: "Entertainment",
-      value:getTotalCost(monthExpenses, "Entertainment")/budgetLimit
+      value: getTotalCost(monthExpenses, "Entertainment") / budgetLimit
     },
 
     {
       src: Home,
       name: "Home",
-      value:getTotalCost(monthExpenses, "Home")/budgetLimit
+      value: getTotalCost(monthExpenses, "Home") / budgetLimit
     },
 
     {
       src: Groceries,
       name: "Other",
-      value: getTotalCost(monthExpenses, "Other")/budgetLimit
+      value: getTotalCost(monthExpenses, "Other") / budgetLimit
     },
   ];
-<<<<<<< HEAD
-=======
 
   function extractKeyValue(obj) {
     let count = 0;
@@ -55,7 +53,6 @@ const CategorySection = ({ monthExpenses,budgetLimit}) => {
     )
     return count;
   }
->>>>>>> 5f41c16d47ec82b74e20da0727c7abe100d106d6
 
   const eachFlexSection = (data, i) => {
     const { src, name, value } = data[1];
