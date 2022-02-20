82a9c649faf311c5cc31c9a1f6bf38d6c875ef6c
idankario <idankario@gmail.com>
2022-02-20 07:40:08 +0200

    lo

--- server/server.js
+++ server/server.js
@@ -9,11 +9,18 @@ const port = process.env.PORT || 8000;
 const { UsersRouter } = require('./routers/usersRouter');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+// Server static assets 
+app.all('*', function (req, res, next) {
+  if (!req.get('Origin')) return next();
+  res.set('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,x-auth-token,x-api-key');
+  next();
+});
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
-app.use(cors());
 app.use('/api/users', UsersRouter);
 
 app.use((req, res) => {
